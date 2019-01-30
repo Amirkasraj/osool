11d05a91dc77108cde1a1c297f56ba9a730c2416
mhno <makhsoosi7@gmail.com>
2019-01-30 15:24:34 +0330

    ss9

--- src/compiler/ALU.java
+++ src/compiler/ALU.java
@@ -23,9 +23,63 @@ public class ALU extends Module {
 		int rd_data = input.get("rd_Data");
 		// int pc+4
 		//int other control lines
+		int second_in=Reg_data2;
+		int ALU_output=0b0;
+		int zero_control=0;
+		if (ALU_src==1)
+			second_in=offset_data;
+		int func=input.get("func");
+		switch (ALU_control(ALU_op1,ALU_op2,func)){
+			case "add":
+				ALU_output=Reg_data1+second_in;
+			break;
+			case "subtract":
+				ALU_output=Reg_data1-second_in;
+				if (ALU_output==0b0)
+					zero_control=1;
+			break;
+			case "and":
+				ALU_output=Reg_data1&second_in;
+			break;
+			case "or":
+				ALU_output=Reg_data1|second_in;
+			break;
+			case "slt":
+				if (Reg_data1 < second_in){
+					ALU_output=0b1;
+				}
+			break;
+		}
+
+		if (Reg_Dst==1){
+			//TODO multi....
+		}
+
+
 
 		// TODO jam o zarb o ina;
 		return null;
 	}
+	public String ALU_control(int ALU_op1,int ALU_op2,int func){
+		if (ALU_op1==0){
+			if (ALU_op2==0){
+				return "add";
+			return "subtract";
+		}else {
+			if (ALU_op2==0){
+				if (func==0b100000)
+					return "add";
+				if (func==0b100010)
+					return "subtract";
+				if (func==0b100100)
+					return "and";
+				if (func==0b100101)
+					return "or";
+				if (func==0b101010)
+					return "slt";
+			}
+		}
+	}
+
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -18,5 +18,6 @@ public class Main {
                 cpu.clock();
             System.out.println(cpu.log());
         }
+
     }
 }
